2923d53837e028e23c6a80b8aec8f19bdd0ffcb1
Trung <trungpnt0605@gmail.com>
2021-06-16 23:15:46 +0700

    added class to bind the data in application.props and use it for the response data of limits

--- microservice-practice/limits-service/limits-service/src/main/java/com/microservicepractice/limitsservice/controller/LimitsController.java
+++ microservice-practice/limits-service/limits-service/src/main/java/com/microservicepractice/limitsservice/controller/LimitsController.java
@@ -1,9 +1,18 @@
 package com.microservicepractice.limitsservice.controller;
 
+import com.microservicepractice.limitsservice.configuration.Configuration;
 import com.microservicepractice.limitsservice.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
-    public Limits
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping ("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(configuration.getMaximum(), configuration.getMinimum());
+    }
 }
