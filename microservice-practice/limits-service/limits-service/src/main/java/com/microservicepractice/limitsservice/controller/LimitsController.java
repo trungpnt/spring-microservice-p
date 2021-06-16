573f48f4ca8644077dbda016ee0dbc0177fc64fd
Trung <trungpnt0605@gmail.com>
2021-06-16 23:07:23 +0700

    added key-value pairs for the service

---
+++ microservice-practice/limits-service/limits-service/src/main/java/com/microservicepractice/limitsservice/controller/LimitsController.java
@@ -0,0 +1,9 @@
+package com.microservicepractice.limitsservice.controller;
+
+import com.microservicepractice.limitsservice.model.Limits;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+    public Limits
+}
---
+++ microservice-practice/limits-service/limits-service/src/main/java/com/microservicepractice/limitsservice/model/Limits.java
@@ -0,0 +1,31 @@
+package com.microservicepractice.limitsservice.model;
+
+public class Limits {
+    private int maximum;
+    private int minimum;
+
+    public Limits(){
+
+    }
+
+    public Limits(int maximum, int minimum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+}
